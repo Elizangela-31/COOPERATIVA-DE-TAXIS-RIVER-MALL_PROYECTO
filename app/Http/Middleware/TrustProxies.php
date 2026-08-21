efa86aa5146f4f9bdeb78203cb8fac918e0854e3
Elizangela-31 <lesanolizzitta31@gmail.com>
2026-08-20 20:46:47 -0500

    Set all proxies as trusted in TrustProxies middleware

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = '*';
 
     /**
      * Encabezados utilizados para detectar servidores proxy.
